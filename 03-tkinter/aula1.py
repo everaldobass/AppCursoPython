92a024a9cbb944434665663feea6d50546467f13
everaldobass <everaldobass@gmail.com>
2021-01-31 00:44:55 -0300

    Atualizando curso

--- 03-tkinter/aula1.py
+++ 03-tkinter/aula1.py
@@ -2,4 +2,17 @@ from tkinter import *
 
 app = Tk()
 
+class Application():
+    def __init__(self):
+        self.root = root
+        self.tela()
+
+        root.mainloop()
+
+    def tela(self):
+        self.root.title("Cadastro de Clientes")
+        self.root.geometry(width=800, heigth=400)
+        
+    
+
 app.mainloop()
