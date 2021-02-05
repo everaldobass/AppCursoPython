68fd736b1901c2dee618975653fa10797539f7e1
everaldobass <everaldobass@gmail.com>
2021-02-04 22:28:21 -0300

    crud django update

--- 06-crud-jango/app/views.py
+++ 06-crud-jango/app/views.py
@@ -2,14 +2,8 @@ from django.shortcuts import redirect, render
 from app.forms import CarrosForm
 from app.models import Carros
 
-# Importaou o codigo abaixo
-#from django.http import HttpResponse
-
-# Criou o arquivo abaixo
-
 
 def home(request):
-    # return HttpResponse('Olá Mundo!')
     # Cria o Retorno abaixo
     data = {}
     data['db'] = Carros.objects.all()
@@ -31,6 +25,23 @@ def create(request):
         return redirect('home')
 
 
-def view(request):
+def view(request, pk):
+    data = {}
     data['db'] = Carros.objects.get(pk=pk)
-    return render(request, 'view.html', data)
+    return render(request,'view.html', data)
+
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    data['form'] = CarrosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    form = CarrosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
--- 06-crud-jango/project/urls.py
+++ 06-crud-jango/project/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 # import home
-from app.views import home, form, create, view
+from app.views import home, form, create, view, edit, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +25,8 @@ urlpatterns = [
     path('form/', form, name='form'),
     path('create/', create, name='create'),
     path('view/<int:pk>/', view, name='view'),
+    path('edit/<int:pk>/', edit, name='edit'),
+    path('update/<int:pk>/', update, name='update')
+
+
 ]
